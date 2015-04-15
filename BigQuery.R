d547a7c5791fb49a16f6175c2930dddc69d78d95
Daniel M Sheehan <daniel.martin.sheehan@gmailcom>
2015-04-15 15:59:06 -0400

    update

--- BigQuery.R
+++ BigQuery.R
@@ -6,5 +6,10 @@ project <- "dazzling-will-91618" # put your project ID here
 sql <- "SELECT * FROM [dazzling-will-91618:taxitest.taxi5] LIMIT 200"
 query_exec(sql, project = project)
 
-# Access code:
+# COPY AND PASTE THE LINK THEY GIVE YOU TO YOUR BROWSER AND THEN ACCEPT WITH LOGIN TO GOOGLE AND PASTE THE AUTHORIZATION CODE THEY GIVE YOU
+
+# Authorization - Danny - code:
+# iMac
 # 4/OhK2GvHVghtg-fXt5_WBGrTTQ3oP0emV6mstUkubJF0.UuWMWrqz-SEUJvIeHux6iLaNNwR9mQI
+# MacBook Pro
+# 4/rqJStTba7RLNXaFpLsuhl87Mw7uzedsxEV3vsLQalP0.YhPJBEFsNmYbgtL038sCVntpOU99mQI
